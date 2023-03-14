e119110f38a4255986ae4123a41e8b4177cc3282
flowered_pants <rikydo@mail.ru>
2023-03-15 01:35:07 +0530

    test which works

--- Registration_form/conftest.py
+++ Registration_form/conftest.py
@@ -1,12 +1,15 @@
 import pytest
 import selene
+from selene import browser
+from selene import have, be
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_managment():
-    selene.browser.config.hold_browser_open = True
-    selene.browser.config.browser_name = 'firefox'
-    selene.browser.config.window_width = 700
-    selene.browser.config.window_height = 7000
-    selene.browser.config.base_url = ('https://demoqa.com')
+    browser.config.hold_browser_open = True
+    browser.config.type_by_js = True
+    browser.config.browser_name = 'firefox'
+    browser.config.window_width = 700
+    browser.config.window_height = 7000
+    browser.config.base_url = ('https://demoqa.com')
     yield
--- Registration_form/registration_form.py
+++ Registration_form/registration_form.py
@@ -1,26 +1,47 @@
+import os
 from selene import browser
 from selene import have, be
+from selenium.webdriver import Keys
 from selenium.webdriver.common import keys
-import pytest
+
 
 def test_registration_form(browser_managment):
     browser.open('/automation-practice-form')
     browser.should(have.title('DEMOQA'))
-    browser.element('[id="firstName"]').should(be.blank).type('Evgeniia')
-    browser.element('[id="lastName"]').should(be.blank).type('Belikova')
-    browser.element('[id="userEmail"]').should(be.blank).type('mur@loc.ru')
+    browser.element('#firstName').should(be.blank).type('Evgeniia')
+    browser.element('#lastName').should(be.blank).type('Belikova')
+    browser.element('#userEmail').should(be.blank).type('mur@loc.ru')
     browser.element('[for="gender-radio-1"]').should(be.clickable)
     browser.element('[for="gender-radio-2"]').should(be.clickable).click()
     browser.element('[for="gender-radio-3"]').should(be.clickable)
-    browser.element('[id="userNumber"]').should(be.blank).type('88002000600')
-    browser.element('[id= "dateOfBirthInput"]').type(keys.COMMAND + 'a' + keys.NULL + '31.03.1997').press_enter()
+    browser.element('#userNumber').should(be.blank).type('88002000600')
+    browser.driver.execute_script('window.scrollBy(0, 300)')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="1997"]').click()
+    browser.element('.react-datepicker__day--031').click()
+    browser.element('#subjectsInput').should(be.blank).type('text')
     browser.element('[for="hobbies-checkbox-1"]').should(be.clickable).click()
     browser.element('[for="hobbies-checkbox-2"]').should(be.clickable).click()
     browser.element('[for="hobbies-checkbox-3"]').should(be.clickable).click()
-    browser.element('[id="currentAddress"]').should(be.blank).type('Moscow, Russia').press_enter()
-    browser.element('[id="close-fixedban"]').click()
-    browser.element('[id="submit"]').click()
-    browser.element('[id="example-modal-sizes-title-lg"]').should(have.text('Thanks for submitting the form'))
-
-def one_more():
-    browser.open()
+    browser.element('#uploadPicture').send_keys((os.getcwd()+"/picture/pic.png"))
+    browser.element('#currentAddress').should(be.blank).type('Moscow, Russia').press_enter()
+    browser.driver.execute_script('window.scrollBy(0, 500)')
+    browser.element('#close-fixedban').click()
+    browser.element('#state').click()
+    browser.element('#react-select-3-option-1').click()
+    browser.element('#city').click()
+    browser.element('#react-select-4-option-2').click()
+    browser.element('#submit').click()
+#проверки
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('//tbody/tr[1]/td[2]').should(have.text('Evgeniia Belikova'))
+    browser.element('//tbody/tr[2]/td[2]').should(have.text('mur@loc.ru'))
+    browser.element('//tbody/tr[3]/td[2]').should(have.text('Female'))
+    browser.element('//tbody/tr[4]/td[2]').should(have.text('8800200060'))
+    browser.element('//tbody/tr[5]/td[2]').should(have.text('31 March,1997'))
+    #browser.element('//tbody/tr[6]/td[2]').should(be.blank)
+    browser.element('//tbody/tr[7]/td[2]').should(have.text('Sports, Reading, Music'))
+    browser.element('//tbody/tr[8]/td[2]').should(have.text('pic.png'))
+    browser.element('//tbody/tr[9]/td[2]').should(have.text('Moscow, Russia'))
+    browser.element('//tbody/tr[10]/td[2]').should(have.text('Uttar Pradesh Merrut'))
